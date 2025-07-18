41f6b53c326b4f1f392ebabc30c5ea20787bcfc5
Will Fox <william.macdonald.fox@gmail.com>
2025-07-17 21:34:14 -0400

    refactor: fix JWT payload syntax errors in Sessions Controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::SessionsController < ApplicationController
     user = User.find_by(username: params[:username])
 
     if user&.authenticate(params[:password])
-      token = JsonWebToken.encode(userid: user_id)
+      token = JsonWebToken.encode(user_id: user.id)
       render json: { message: "Welcome back, #{user.username}!", token: token}, status: :ok 
     else
       render json: { error: "Invalid credentials" }, status: :unauthorized
