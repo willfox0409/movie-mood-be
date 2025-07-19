d388084039020b1447812ae2b074bb56afa854ff
Will Fox <william.macdonald.fox@gmail.com>
2025-07-19 17:08:01 -0400

    feat: initialize tmdbService, tested in Postman

--- app/services/tmdb_service.rb
+++ app/services/tmdb_service.rb
@@ -1,3 +1,37 @@
-class TMDBService
-  
+require 'httparty'
+
+class TmdbService
+  include HTTParty
+  base_uri "https://api.themoviedb.org/3"
+
+  def self.search_movie(title)
+    puts "🔍 TmdbService.search_movie called with title: #{title}"
+
+    response = self.get("/search/movie", query: {
+      query: title, 
+      api_key: ENV["TMDB_API_KEY"]
+    })
+
+    puts "🔍 Response class: #{response.class}"
+
+    return nil unless response.success? && response["results"].present?
+
+    response["results"].first
+  end
+
+  def self.movie_details(tmdb_id)
+    response = self.get("/movie/#{tmdb_id}", query: {
+      api_key: ENV["TMDB_API_KEY"]
+    })
+
+    puts "🔍 Response class: #{response.class}"
+
+    return nil unless response.success?
+
+    response.parsed_response
+  end
+
+  def self.full_poster_url(path)
+    "https://image.tmdb.org/t/p/w500#{path}"
+  end
 end
