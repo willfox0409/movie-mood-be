487ce44525c651e8ac9cf2b3481735298b9be9c1
Will Fox <william.macdonald.fox@gmail.com>
2025-07-19 17:06:56 -0400

    feat: build Recommendation serializer

--- app/serializers/recommendation_serializer.rb
+++ app/serializers/recommendation_serializer.rb
@@ -1,13 +1,13 @@
 class RecommendationSerializer
   include JSONAPI::Serializer
   attributes :user_id,
-            :mood,
-            :genre,
-            :decade,
-            :runtime,
-            :recommended_title,
-            :tmdb_id,
-            :openai_prompt,
-            :openai_response,
-            :recommended_at
+              :mood,
+              :genre,
+              :decade,
+              :runtime,
+              :recommended_title,
+              :tmdb_id,
+              :openai_prompt,
+              :openai_response,
+              :recommended_at
 end
