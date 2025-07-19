40bc6ed2afdc22179649c22d7a55b513293e84b9
Will Fox <william.macdonald.fox@gmail.com>
2025-07-18 22:59:20 -0400

    feat: generate RecommendationSerializer using jsonapi gem

---
+++ app/serializers/recommendation_serializer.rb
@@ -0,0 +1,13 @@
+class RecommendationSerializer
+  include JSONAPI::Serializer
+  attributes :user_id,
+            :mood,
+            :genre,
+            :decade,
+            :runtime,
+            :recommended_title,
+            :tmdb_id,
+            :openai_prompt,
+            :openai_response,
+            :recommended_at
+end
