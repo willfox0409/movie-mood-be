8371e7d978bf91e9877aa950fc990a0625ffdbae
Will Fox <william.macdonald.fox@gmail.com>
2025-08-06 16:23:32 -0400

    config: add factorybot syntax methods

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -45,6 +45,10 @@ RSpec.configure do |config|
     Rails.root.join('spec/fixtures')
   ]
 
+  RSpec.configure do |config|
+    config.include FactoryBot::Syntax::Methods
+  end
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
