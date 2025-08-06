1add02e2c966e6817165a5d4b59f4d4b72e56581
Will Fox <william.macdonald.fox@gmail.com>
2025-08-06 16:29:10 -0400

    feat: add has_many saved_movies association to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
-  has_many :recommendations
+  has_many :recommendations, dependent: :destroy
+  has_many :saved_movies, dependent: :destroy
 
   validates :email, presence: true
   validates :username, presence: true 
